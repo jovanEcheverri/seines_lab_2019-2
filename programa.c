44c44226b0b6f31fa1bd3409b4137ce8ad4f77eb
tigarto <helpu1409@gmail.com>
2019-12-02 07:08:29 -0500

    Agregando primer programa

---
+++ programa.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Declaraciones de funciones
+// Variables globales
+
+int main() {
+  // Variables locales
+  // Codigo
+  printf("Entonces que gonorrea!!!!\n");
+
+  return 0;
+}
