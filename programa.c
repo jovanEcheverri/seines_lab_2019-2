b2662e957f1235759ae4e9ad692173ef72334b62
tigarto <helpu1409@gmail.com>
2019-12-02 07:15:20 -0500

    Actualizando primer programa

--- programa.c
+++ programa.c
@@ -1,12 +1,35 @@
 #include <stdio.h>
 
+/*
+Para compilar:
+gcc programa.c -o xxx
+*/
+
 // Declaraciones de funciones
+void saludos(int n);
+
 // Variables globales
 
+// Funcion main
 int main() {
   // Variables locales
   // Codigo
-  printf("Entonces que gonorrea!!!!\n");
-
+  printf("Entonces que gonorrea!!!!\n\n");
+  saludos(-6);
+  printf("\n");
+  saludos(3);
+  printf("\n");
   return 0;
+}
+
+// Otras funciones
+void saludos(int n) {
+  if (n < 0) {
+    printf("Maleducado\n");
+  }
+  else {
+    for(int i = 0; i < n; i++) {
+      printf("Quiubo marica!!!!\n");
+    }
+  }
 }
